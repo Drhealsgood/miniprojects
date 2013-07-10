c46105eebe5abdfd570aa01c27459a6f86772289
Luke <doclukemckee@gmail.com>
2013-07-10 12:28:58 +1200

    add tests for counting vowels and check is_palindrome

--- text_projects/string_editing_tests.py
+++ text_projects/string_editing_tests.py
@@ -20,6 +20,26 @@ class StringEditingTests(unittest.TestCase):
         and returns the string reversed
         """
         orig        = "Hello World!"
-        expected    = "".join(reversed(orig))
+        expected    = "".join(reversed(orig)) # Must remember reversed() returns a reversed object - iterable
         output      = self.myString.reverse(orig)
         self.assertEquals(output, expected, "{0} expected, but got {1}".format(expected, output))
+        
+    def test_count_vowels(self):
+        """
+            Count vowels will take a string and return the amount of vowels contained in the string
+        """
+        some_string     = "testing one two three" # 7 vowels
+        no_vowels       = 7
+        actual          = self.myString.count_vowels(some_string)
+        self.assertEqual(actual, no_vowels, "{0} expected, but got {1}".format(no_vowels, actual))
+        
+    def test_is_palindrome(self):
+        """
+            Returns true if the passed argument is a palindrome
+        """
+        for phrase in ["mum mum", "racecar"]:
+            self.assertTrue(self.myString.is_palindrome(phrase))
+        self.assertFalse(self.myString.is_palindrome("not_a_palindrome"))
+        
+        
+        
