8cb06459ae7349492523b3cc0c03d7e226221ecb
Luke <doclukemckee@gmail.com>
2013-07-10 12:18:53 +1200

    reverse test and function completion

--- text_projects/string_editing_tests.py
+++ text_projects/string_editing_tests.py
@@ -4,13 +4,15 @@ Created on 10/07/2013
 @author: luke
 '''
 import unittest
-from string_editting import MyString
+from string_editting import *
 
 class StringEditingTests(unittest.TestCase):
     """
     StringEditing will contain a bunch of methods to edit
     and play with strings
     """
+    def setUp(self):
+        self.myString = MyString()
     
     def test_reverse(self):
         """
@@ -18,6 +20,6 @@ class StringEditingTests(unittest.TestCase):
         and returns the string reversed
         """
         orig        = "Hello World!"
-        expected    = reversed(orig)
-        output      = Mystring.reverse(orig)
+        expected    = "".join(reversed(orig))
+        output      = self.myString.reverse(orig)
         self.assertEquals(output, expected, "{0} expected, but got {1}".format(expected, output))
--- text_projects/string_editting.py
+++ text_projects/string_editting.py
@@ -4,5 +4,10 @@ Created on 10/07/2013
 @author: luke
 '''
 
+class MyString():
+    
+    def reverse(self, to_rev):
+        return to_rev[::-1]
+
 if __name__ == '__main__':
     pass
