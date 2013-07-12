cd2b086b7596234e35c83d7f594629813d9f5772
Luke <doclukemckee@gmail.com>
2013-07-13 01:45:26 +1200

    conversion tests

---
+++ number_projects/conversion/conversions.py
@@ -0,0 +1,5 @@
+'''
+Created on 12/07/2013
+
+@author: luke
+'''
--- number_projects/conversion/conversions_tests.py
+++ number_projects/conversion/conversions_tests.py
@@ -5,6 +5,8 @@ Created on 12/07/2013
 '''
 import unittest
 import conversions
+import json
+import urllib.request
 
 
 class TestConverter(unittest.TestCase):
@@ -24,13 +26,32 @@ class TestConverter(unittest.TestCase):
         for bin,val in tests:
             self.assertEquals(decToBin(val), bin)
             
-    def testCelsiusToX(self):
-        pass
-    
-    def testFarenheitToX(self):
-        pass
-    
-    def testKelvinToX(self):
+    def testTemperatureConversion(self):
+        conversion  = lambda msr_from, msr_to, amt: self.converter.tempConvert(msr_from, msr_to, amt)
+        celToFar    = conversion('celsius','farenheit',30)
+        celToKel    = conversion('celsius', 'kelvin',30)
+        farToCel    = conversion('farenheit', 'celsius',100)
+        farToKel    = conversion('farenheit', 'kelvin',100)
+        kelToCel    = conversion('kelvin','celsius',300)
+        kelToFar    = conversion('kelvin','farenheit',300)
+        self.assertEquals(celToFar,86)
+        self.assertEquals(celToKel, 303.15)
+        self.assertEquals(farToCel,37.778)
+        self.assertEquals(farToKel,310.928)
+        self.assertEquals(kelToCel,26.85)
+        self.assertEquals(kelToFar,80.33)
+        
+        
+        
+    def testCurrencyExchange(self):
+        curr_exchange   = lambda c_one, c_two, amount: self.converter.currencyExchange(c_one,c_two,amount)
+        exg_amt         = curr_exchange('USD','NZD',50)
+        curr_page       = urllib.request.urlopen('http://openexchangerates.org/api/latest.json?app_id=9f0710764c064370932f4f2496968c62')
+        curr_details    = json.loads(curr_page)
+        usd,nzd         = curr_details['USD'], curr_details['NZD']
+        convert_amt     = nzd/usd
+        result          = convert_amt*50
+        self.assertEqual(exg_amt, result) 
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
