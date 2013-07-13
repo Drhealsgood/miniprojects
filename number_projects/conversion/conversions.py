5d9af5b16c4f30797151b9bc80a02f3128ba8672
Luke <doclukemckee@gmail.com>
2013-07-13 21:54:18 +1200

    test bugs for converter and class initialisation for converter

--- number_projects/conversion/conversions.py
+++ number_projects/conversion/conversions.py
@@ -3,3 +3,38 @@ Created on 12/07/2013
 
 @author: luke
 '''
+
+class Converter():
+    
+    def binToDec(self, bin_no):
+        """
+        @param bin_no:  a string representation of a binary number
+        @return:        an integer value of the binary number passed
+        """
+        pass
+    
+    def decToBin(self, dec_no):
+        """
+        @param dec_no:  an integer value 
+        @return:        a string representation of the decimal value passed
+        """
+        pass
+    
+    def currencyExchange(self,con_from,con_to,value):
+        """
+        @param con_from:a string representation of the currency to convert from
+        @param con_to:  a string representation of the currency to convert to
+        @param value:   amount to convert
+        @return:        the value of the conversion from con_from to con_to of value
+        """
+        pass
+    
+    def tempConvert(self,msr_from,msr_to,amt):
+        """
+        @param msr_from:a string representation of the measurement to convert from
+        @param msr_to:  a string representation of the measurement to convert to
+        @param amt:     the value to convert
+        @return:        the converted temperature value
+        """
+        pass
+    pass
--- number_projects/conversion/conversions_tests.py
+++ number_projects/conversion/conversions_tests.py
@@ -15,14 +15,14 @@ class TestConverter(unittest.TestCase):
     def testBinaryToDecimal(self):
         binToDec    = lambda value: self.converter.binToDec(value) 
         tests       = {'1010':10,'0010':2,'00011010':26}
-        for bin,val in tests:
-            self.assertEqual(binToDec(bin), val)
+        for bin in tests:
+            self.assertEqual(binToDec(bin), tests[bin])
 
     def testDecimalToBinary(self):
         decToBin    = lambda value: self.converter.decToBin(value)
         tests       = {'1010':10,'0010':2,'00011010':26}
-        for bin,val in tests:
-            self.assertEquals(decToBin(val), bin)
+        for bin in tests:
+            self.assertEquals(decToBin(tests[bin]), bin)
             
     def testTemperatureConversion(self):
         conversion  = lambda msr_from, msr_to, amt: self.converter.tempConvert(msr_from, msr_to, amt)
@@ -32,6 +32,7 @@ class TestConverter(unittest.TestCase):
         farToKel    = conversion('farenheit', 'kelvin',100)
         kelToCel    = conversion('kelvin','celsius',300)
         kelToFar    = conversion('kelvin','farenheit',300)
+        self.assertRaises(TypeError, conversion('asg','another',300))
         self.assertEquals(celToFar,86)
         self.assertEquals(celToKel, 303.15)
         self.assertEquals(farToCel,37.778)
@@ -43,6 +44,7 @@ class TestConverter(unittest.TestCase):
         curr_exchange   = lambda c_one, c_two, amount: self.converter.currencyExchange(c_one,c_two,amount)
         exg_amt         = curr_exchange('USD','NZD',50)
         curr_page       = urllib.request.urlopen('http://openexchangerates.org/api/latest.json?app_id=9f0710764c064370932f4f2496968c62')
+        print(curr_page.read())
         curr_details    = json.loads(curr_page)
         usd,nzd         = curr_details['USD'], curr_details['NZD']
         convert_amt     = nzd/usd
