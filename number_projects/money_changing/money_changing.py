f52a6fd91df08b0f356a828713adc7d25a84e774
Luke <doclukemckee@gmail.com>
2013-07-14 01:46:09 +1200

    .

--- number_projects/money_changing/money_changing.py
+++ number_projects/money_changing/money_changing.py
@@ -15,34 +15,3 @@ def greedy_money_exchange(denom,amount):
         amount  = amount-num*denom[i] # set new amount
         i       = i + 1 # go to next denom
     return used
-        
-def dynamic_money_exchange(denom,amount):
-    """
-    this is buggy
-    @todo: fix the bugginess
-    @param denom:     takes a bunch of denominators
-    @param amount:    amount to build in denominators
-    @return:          tuple(amountofcoins,whichcoins)
-    """
-    table       = [[0 for i in range(amount+1)] for i in range(len(denom))] # construct table
-    table[0]    = range(amount+1)                                           # set value table
-    used        = [0 for val in denom]                                      # used denoms
-    for i in range(1,len(denom)): # go over denoms
-        for j in range(0,amount+1):  # for each value up to amount
-            if j < denom[i]:         # if the value is less than the denom
-                # set denom i position j to the value in the previous denom at position j
-                table[i][j] = table[i-1][j]
-            else:
-                values = (table[i-1][j], 1 + table[i][j-denom[i]])
-                table[i][j] = min(values)
-    print(table)
-    return table[-1][-1]
-        
-
-if __name__ == '__main__':
-    print(greedy_money_exchange([10,5,1],12))
-    print(greedy_money_exchange([10,5,1],56))
-    print(greedy_money_exchange([10,6,1],12)) # obviously [0,2,0] more appropriate
-    print(dynamic_money_exchange([10,6,1],12))
-    print(dynamic_money_exchange([10,5,1],12))
-    print(dynamic_money_exchange([10,5,1],56))
