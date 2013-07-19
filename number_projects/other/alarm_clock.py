d75a8d111a45419e68ac23a7070acc503db054ee
Luke <doclukemckee@gmail.com>
2013-07-20 02:03:24 +1200

    .

--- number_projects/other/alarm_clock.py
+++ number_projects/other/alarm_clock.py
@@ -5,34 +5,34 @@ Created on 18/07/2013
 @todo: pyglet doesn't work with python 3 :(
 '''
 import time
-import pyglet
+from subprocess import Popen
 
 class Alarm(object):
     '''
     classdocs
     '''
-    _alarm          = pyglet.media.load('/home/luke/Music/Saints Row The Third - The Soundtrack')
-
-    def timer(self,time):
+    _sound        = '/home/luke/Music/Saints Row The Third - The Soundtrack'
+    
+    
+    def timer(self,timex):
         """
         time = time in seconds since epoch
         """
-        activate    = time.time()+time
+        activate    = time.time()+timex
         while time.time() < activate:
-            pass
-        self._alarm.play()
-        pyglet.app.run()
+            print(time.time(),activate)
+        print("out of loop")
+        Popen(["vlc",self._sound])
         
-    def alarm(self,time):
+    def alarm(self,timex):
         """
         Time = time in seconds since epoch to set it off
         """
-        while time.time() < time:
-            pass
-        self._alarm.play()
-        pyglet.app.run()
+        time.sleep(timex-time.time())
+        print("out of loop")
+        Popen(["vlc",self._sound])
         
 if __name__ == "__main__":
     x       = Alarm()
-    x.timer(time.time()+10)
-        
+    y = "Saturday 20 July 02:00:00 2013"
+    print (time.mktime(y))
