b686fdf7906763b6b38ab5833e3d12901a66be5f
lom2 <lom2@cms-r6-63.cms.waikato.ac.nz>
2015-06-15 23:40:17 +1200

    public Remove and Search implemented with private helper locate

--- datastructures_and_algorithms/datastructures/Dictionaries/Dictionary.java
+++ datastructures_and_algorithms/datastructures/Dictionaries/Dictionary.java
@@ -24,6 +24,65 @@ public class Dictionary<K, V> implements IDictionary<K, V>, Serializable
 	    
 	} // END CONSTRUCTOR
 
-    
+    public V getValue(K key)
+    {
+	V result = null;
+
+	int index = this.getHashIndex(key);
+	index = this.locate(index, key);
+
+	if (index != -1)
+	    {
+		result = this._table[index];
+	    }
+	return result;
+    }
+
+    public V remove(K key)
+    {
+	V removedValue = null; // assume no value has been removed
+	int index = this.getHashKey(key);  // get original insertion point for entry
+	index = this.locate(index, key); // probe until found
+
+	if (index != -1)
+	    {
+		// get the value to be removed and then remove value from dictionary
+		removedValue = this._table[index].getValue();
+		this.setToRemoved(index);
+		this._numberOfEntries--; // reduce amount of entries
+	    }
+	// return removedValue as null or value being removed
+	return removedValue;
+    }
+
+    /** 
+     * Suporter function for methods searching for a value in the dictionary.
+     * @param index      Current index searching from
+     * @param key        searching for
+     * @returns index    of value if found otherwise -1 if not in dictionary.
+     */
+    private int locate(int index, K key)
+    {
+	boolean found = false;
+	int result = -1; // assume not in table here for tidier code
+	// search until key is found 
+	while (!found && (hashTable[index] != null)
+	       {
+		   if (hashTable[index].isIn() && 
+		       key.equals(hashTable[index].getKey()) )
+		       {
+			   found = true;
+		       }
+		   else
+		       {
+			   // probe to next location
+			   index = this._linearProbe(index) % this._table.length;
+		       }
+	       }
+	       // either key is found or null is found at index location
+	       if(found)
+		   result = index;
+	       return result;
+    }
 
 }
